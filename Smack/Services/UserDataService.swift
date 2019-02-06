e864fc668ca253c86b50ab99226f16431510b451
Chris Parr <parr.cj@gmail.com>
2019-02-06 19:03:26 +0000

    lesson 12.

---
+++ Smack/Services/UserDataService.swift
@@ -0,0 +1,33 @@
+//
+//  UserDataService.swift
+//  Smack
+//
+//  Created by Christopher Parr on 06/02/2019.
+//  Copyright © 2019 Christopher Parr. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    static let instance = UserDataService()
+    
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+    
+    func setUserData(id: String, color: String, avatarName: String, email: String, name: String){
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+    }
+    
+    func setAvatarName(avatarName: String){
+        self.avatarName = avatarName
+    }
+    
+}
