67ad44c4140c39b30a91e909561875e5bb34ce1c
Chris Parr <parr.cj@gmail.com>
2019-02-06 18:22:39 +0000

    lesson 11.

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -29,9 +29,16 @@ class CreateAccountVC: UIViewController {
         guard let pass = passTxt.text , passTxt.text != "" else { return }
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            
             if success {
-                print("registered user!")
+                AuthService.instance.loginUser(email: email, password: pass
+                    , completion: { (success) in
+                        if success {
+                            print("logged in user!", AuthService.instance.authToken)
+                        }
+                })
             }
+            
         }
     }
     
--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -45,17 +46,13 @@ class AuthService {
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler){
         
         let lowerCaseEmail = email.lowercased()
-        
-        let header = [
-            "Content-Type": "application/json; charset=utf-8"
-        ]
-        
+    
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             
             if response.result.error == nil {
                 completion(true)
@@ -66,6 +63,36 @@ class AuthService {
             
         }
     }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+        
+            if response.result.error == nil {
+            
+                guard let data = response.data else { return }
+                let json = try! JSON(data: data)
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
+                
+                self.isLoggedIn = true
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+            
+        }
+        
+    }
+    
     
     
     
