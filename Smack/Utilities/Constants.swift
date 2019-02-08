00ad73dcad3330e583aa7f5e8d4d17e2c612a6e8
Chris Parr <parr.cj@gmail.com>
2019-02-08 10:18:45 +0000

    Lesson 15.

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -16,6 +16,12 @@ let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
 
+//Colours
+let smackPurplePlaceholder = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5037457192)
+
+//Notification Constants
+let NOTIF_USER_DATA_DID_CHANGE = Notification.Name(rawValue: "notifUserDataChanged")
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Christopher Parr on 08/02/2019.
+//  Copyright © 2019 Christopher Parr. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView(){
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+}
